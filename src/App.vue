42e6345c702a31929ab5032aa6c3d36f33c38d72
Hassan97-js <hassansyr58@gmail.com>
2021-03-01 08:11:52 +0000

    Added online&offline events in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,51 @@
 <template>
-  <div id="app">
-    <Nav />
-    <router-view />
-  </div>
+    <div id="app">
+        <Nav />
+        <router-view />
+    </div>
 </template>
 
 <script>
 import Nav from "@/components/Global/Nav.vue";
 export default {
-  name: "App",
-  created() {
-    this.getOnlineStatus().then(isOnline => {
-      this.$swal(isOnline ? "You're Online 🙂" : "You're Offline 😔");
-    });
-  },
-  components: {
-    Nav
-  },
-  methods: {
-    async getOnlineStatus() {
-      if (navigator.onLine) {
-        try {
-          await fetch(location.origin, { method: "HEAD" });
-          return true;
-        } catch (e) {
-          return false;
+    name: "App",
+    created() {
+        window.addEventListener("online", () => {
+            this.$swal("You're Online 🙂");
+            console.log("Online Event");
+        });
+        window.addEventListener("offline", () => {
+            this.$swal("You're Offline 😔");
+        });
+        this.getOnlineStatus().then(isOnline => {
+            this.$swal(isOnline ? "You're Online 🙂" : "You're Offline 😔");
+            console.log("Online Event-getOnlineStatus");
+        });
+    },
+    components: {
+        Nav
+    },
+    methods: {
+        async getOnlineStatus() {
+            if (navigator.onLine) {
+                try {
+                    await fetch(location.origin, { method: "HEAD" });
+                    return true;
+                } catch (e) {
+                    return false;
+                }
+            }
+            return new Promise(resolve => resolve(false));
         }
-      }
-      return new Promise(resolve => resolve(false));
     }
-  }
 };
 </script>
 
 <style lang="scss">
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 </style>
